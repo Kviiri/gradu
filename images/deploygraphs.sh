d5126c0fc5554a38bd182448f3e1aaf93ef98010
Kalle Viiri <kalle.viiri@gmail.com>
2018-02-13 11:15:12 +0200

    graph scripts

---
+++ images/deploygraphs.sh
@@ -0,0 +1,2 @@
+circo potenssiverkko.dot |sed 's/}/a -- c [style = dashed]; b -- d [style = dashed];c -- e [style = dashed];d -- f [style = dashed];e -- a [style = dashed];f -- b [style = dashed];}/g' | neato -n -Tpng > potenssiverkko.png
+
