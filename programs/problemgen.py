861181364554f7f353697541ce315f0f29a283f4
Kalle Viiri <kalle.viiri@gmail.com>
2018-10-27 22:55:51 +0300

    first draft of canonization - does NOT work

--- programs/problemgen.py
+++ programs/problemgen.py
@@ -6,7 +6,7 @@ from scipy.misc import comb
 
 exclusionset = {}
 
-# bit numbers for subproblems (0 == most significant)
+# bit numbers for subproblems (0 == least significant)
 # 0 = 00001
 # 1 = 00010
 # 2 = 00011
@@ -22,31 +22,15 @@ exclusionset = {}
 #12 = 01101
 #13 = 01110
 #14 = 01111
-#15 = 11110
-#16 = 11101
-#17 = 11100
-#18 = 11011
-#19 = 11010
-#20 = 11001
-#21 = 11000
-#22 = 10111
-#23 = 10110
-#24 = 10101
-#25 = 10100
-#26 = 10011
-#27 = 10010
-#28 = 10001
-#29 = 10000
-#30 = 11111
-#31 = 00000
-
-#for tiles 0-14, tile+15 is the tile's inversion
+#15 = 10000
+#16 = 10001
+#etc
 
 #first bit is center, then N, E; S, W
 
 #optimizations:
 #1) Culling of trivials. No need to produce any problem where bits 30 or 31 are 1
-#2) Mirror universe culling: No need to produce problems where bits 0-14 are a PROPER superset of 15-29
+#2) Non-canonical culling: No need to solve non-canonical problems
 #3) Superset culling: No need to produce a problem if its subset is solved
 
 #implementations:
@@ -58,21 +42,19 @@ exclusionset = {}
 #input: number of the problem
 #output: tuple ('center', {neighborhood dict})
 def int_to_tile(code):
-    center = '1'
-    neighborhood = {'N': '1', 'E': '1', 'S': '1', 'W': '1'}
-    code = (30 - code) & 31
+    code += 1
+    center = '0'
+    neighborhood = {'N': '0', 'E': '0', 'S': '0', 'W': '0'}
     if code & 16 != 0:
-        center = '0'
-    else:
-        code = (code - 1) ^ 0b1111
+        center = '1'
     if code & 8 != 0:
-        neighborhood['N'] = '0'
+        neighborhood['N'] = '1'
     if code & 4 != 0:
-        neighborhood['E'] = '0'
+        neighborhood['E'] = '1'
     if code & 2 != 0:
-        neighborhood['S'] = '0'
+        neighborhood['S'] = '1'
     if code & 1 != 0:
-        neighborhood['W'] = '0'
+        neighborhood['W'] = '1'
     return (center, neighborhood)
 
 #input: 32 bit integer
@@ -85,8 +67,90 @@ def int_to_problem(code):
             ret.append(int_to_tile(i))
     return ret
 
-def is_mirror_universe(code):
-    return (code >> 15) & (code & 32767) == code & 32767 && code >> 15 != code & 32767
+def problem_to_int(problem):
+    code = 0
+    for tile in problem:
+        tilecode = 0
+        if tile[1]['W'] == '1':
+            tilecode |= 1
+        if tile[1]['S'] == '1':
+            tilecode |= 2
+        if tile[1]['E'] == '1':
+            tilecode |= 4
+        if tile[1]['N'] == '1':
+            tilecode |= 8
+        if tile[0] == '1':
+            tilecode |= 16
+        code |= (2**tilecode)
+    return code
+
+
+#returns whether the code corresponds to the canonical version of the problem
+def is_canonical(code):
+    baseproblem = int_to_problem(code)
+    mirrors = [baseproblem,
+            transform(baseproblem, horizontal_mirror), 
+            transform(baseproblem, vertical_mirror),
+            transform(transform(baseproblem, vertical_mirror), horizontal_mirror)]
+    for mirror in mirrors:
+        inverse = transform(mirror, invert)
+        #print "my code is %s and my stupid inverse is %s" % (str(code), problem_to_int(inverse))
+        if code > problem_to_int(inverse):
+            return False
+        else:
+            print "I seriously consider %s to be simpler than %s" % (mirror, inverse)
+            print "Their respective codes are: %s and %s" % (problem_to_int(mirror), problem_to_int(inverse)) 
+        for problem in [mirror, inverse]:
+            rotations = map(lambda x: rotate_tile(x), problem)
+            firstrot = [i[0] for i in rotations]
+            secondrot = [i[1] for i in rotations]
+            thirdrot = [i[2] for i in rotations]
+            if (code > problem_to_int(firstrot)
+                or code > problem_to_int(secondrot)
+                or code > problem_to_int(thirdrot)):
+                return False
+    return True
+            
+
+#returns a new tile rotated 90 degrees clockwise
+def rotate_tile(tile):
+    ret = []
+    rotations = 3
+    while rotations > 0:
+        newtile = (tile[0], {
+            'N': tile[1]['W'],
+            'E': tile[1]['N'],
+            'S': tile[1]['E'],
+            'W': tile[1]['S']})
+        ret.append(newtile)
+        rotations -= 1
+        tile = newtile
+    return ret 
+
+def horizontal_mirror(tile):
+    return (tile[0], {
+        'N': tile[1]['S'],
+        'E': tile[1]['E'],
+        'S': tile[1]['N'],
+        'W': tile[1]['W']})
+
+def vertical_mirror(tile):
+    return (tile[0], {
+        'N':tile[1]['N'],
+        'E':tile[1]['W'],
+        'S':tile[1]['S'],
+        'W':tile[1]['E']})
+
+def invert(tile):
+    return (str(1 - int(tile[0])),{
+            'N': str(1 - int(tile[1]['N'])),
+            'E': str(1 - int(tile[1]['E'])),
+            'S': str(1 - int(tile[1]['S'])),
+            'W': str(1 - int(tile[1]['W']))})
+
+def transform(prob, func):
+    return [i for i in map(func, prob)]
+
 
 def is_excluded(code):
     for exclusion in exclusionset:
@@ -114,7 +178,7 @@ def main():
                 exclusionset.add(int(line))
     for i in range(first, first+amount):
         problem = get_nth_problem(i, bitcount)
-        if not is_mirror_universe(problem):
+        if is_canonical(problem):
             if not is_excluded(problem):
                 print int_to_problem(problem)
 
