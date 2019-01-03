94034b026f01c813ed91a893c990657c97d7c4fd
Kalle Viiri <kalle.viiri@gmail.com>
2019-01-03 20:46:21 +0200

    fix bug that makes solver too permissive esp. for very difficult problems

--- programs/lightweight_solver.py
+++ programs/lightweight_solver.py
@@ -14,7 +14,6 @@ with open(argv[2]) as f:
     graphdef = defaultdict(list)
     for node in map(lambda x: literal_eval(x), f.readlines()):
         graphdef[node[0]].append(node[1])
-
 i = 1
 node_id = {}
 for key in graphdef:
@@ -23,22 +22,29 @@ for key in graphdef:
 
 for problemint in problemset:
     clauselist = []
-    #construct set of ineligible tiles
+    #construct set of ineligible candidates
     ineligible = [x for x in int_to_problem(0xFFFFFFFF)
             if x not in int_to_problem(problemint)]
     for node in graphdef:
-        for tile in ineligible:
+        for candidate in ineligible:
             newclause = []
-            if tile[0] == '0':
+            if candidate[0] == '0':
                 newclause.append(node_id[node])
             else:
                 newclause.append(-node_id[node])
 
+            neighbor_contingencies = {'S': [], 'W': [], 'N': [], 'E': []}
             for neighbor in graphdef[node]:
+                #print str(neighbor)
                 neighbor_id = node_id[neighbor[1]]
                 newclause.append(neighbor_id * 
-                        (1 if tile[1][neighbor[0]] == '0' else -1))
+                        (1 if candidate[1][neighbor[0]] == '0' else -1))
+                neighbor_contingencies[neighbor[0]].append(neighbor_id)
+            for direction in neighbor_contingencies:
+                nodelist = neighbor_contingencies[direction]
+                for first, second in zip(nodelist, nodelist[1:]):
+                    clauselist.append([first, -second])
+                    clauselist.append([-first, second])
             clauselist.append(newclause)
     if solve(clauselist) != u'UNSAT':
        print problemint 
-
